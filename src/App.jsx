2e4257ebafd985e1d035ce299157b1d656660a23
abdurazzoq <jasurbekkarimboev997@gmail.com>
2024-12-24 15:57:42 +0500

    qq

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+import rec from "./assets/rec.svg";
+import oodi from "./assets/oodi.svg";
+import phone from "./assets/phone.svg";
+import search from "./assets/search.svg";
+import qiz from "./assets/qiz.svg";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Jasurbek</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        <section className="header">
+          <div className="container">
+            <div className="header-parent">
+              <div className="header-left">
+                <img src={rec} alt="" />
+                <img src={oodi} alt="" />
+              </div>
+              <div className="header-center">
+                <a className="home" href="">
+                  Home
+                </a>
+                <a href="">Menu</a>
+                <a href="">Services</a>
+                <a href="">Offers</a>
+              </div>
+              <div className="header-right">
+                <img src={search} alt="" />
+                <button>
+                  <img src={phone} alt="" />
+                  Contact
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
 
-export default App
+        <section className="main">
+          <div className="container">
+            <div className="main-parent">
+              <div className="main-left">
+                <h1>
+                  Dive into Delights <br /> Of Delectable Food
+                </h1>
+                <p>
+                  Where Each Plate Weaves a Story of Culinary <br /> Mastery and
+                  Passionate Craftsmanship
+                </p>
+                <div className="main-main">
+                  <button>Order Now</button>
+                  <h2>Watch Video</h2>
+                </div>
+              </div>
+              <div className="main-right">
+                <img src={qiz} alt="" />
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
